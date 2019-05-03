9746d79805c2a9a8df17d0ea2157ab93b3604d78
yyfann <a1097833559@163.com>
2019-05-03 11:02:06 +0800

    fc

---
+++ src/math.js
@@ -0,0 +1,13 @@
+
+function min(a, b) {
+  return a - b
+}
+
+module.exports = {
+  add: (a, b) => {
+    return a + b
+  },
+  mul: (a, b) => {
+    return a * b
+  }
+}
---
+++ test/simple.js
@@ -0,0 +1,13 @@
+// const { add, multi } = require('../src/math')
+
+// const { should, expect, assert } = require('chai')
+
+// describe('#math', () => {
+//   describe('add', () => {
+//     it('should return 5 when 2 + 3', () => {
+//       expect(add(2, 3), 5)
+//     })
+//   })
+// })
+
+var a = 1
